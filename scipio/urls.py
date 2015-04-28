8b7107fc64fc245b6af34ce04c69e2d5251d101f
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2015-04-28 19:29:55 +0300

    Bugfixes for Django==1.7.4

--- scipio/urls.py
+++ scipio/urls.py
@@ -1,5 +1,8 @@
-# -*- coding:utf-8 -*-
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    # Django < 1.4
+    from django.conf.urls.defaults import patterns, url
 
 from scipio import views
 
